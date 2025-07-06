56a9761863905636701ca179599d9da59aa181d7
Nicholas Delatorre <58831536+NicktasticDEV@users.noreply.github.com>
2025-07-06 16:14:51 -0400

    Erm... meow
    Not much cool shit.

---
+++ Scripts/Runtime/Utilities/TextureUtils.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace Exprite
+{
+    public class TextureUtils : MonoBehaviour
+    {
+        /*
+        public float GetPixelsPerUnit(Texture2D texture2D)
+        {
+            Sprite tempSprite;
+            Rect spriteRect = new Rect(0, 0, texture2D.width, texture2D.height);
+
+            Sprite.Create(texture2D, spriteRect, new Vector2(0.5f, 0.5f));
+        }
+        */
+    }
+}
